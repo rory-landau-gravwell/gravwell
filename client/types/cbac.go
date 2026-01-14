1da571c408de424f40053eb0ef2fe96b3d4a25fc
Nic O'Sullivan <nic.osullivan@gravwell.io>
2026-01-14 12:12:13 -0600

    add type comment

--- client/types/cbac.go
+++ client/types/cbac.go
@@ -993,12 +993,14 @@ type TokenCreate struct {
 	Capabilities []string  `json:"capabilities"`
 }
 
+// TokenRegeneration is the structure used to request regeneration of an existing token
 type TokenRegeneration struct {
 	Expires time.Time `json:"expiresAt,omitempty"`
 }
 
 // TokenFull represents the response value for a token create request
-// this type is the only type that contains the token value and is ONLY provided when creating a new token
+// this type is the only type that contains the token value and is
+// ONLY provided when creating a new token or regenerating a token
 type TokenFull struct {
 	Token
 	Value string `json:"token"`
