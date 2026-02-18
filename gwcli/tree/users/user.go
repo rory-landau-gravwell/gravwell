07e067a56ba90a84e68d90fdc68f9ae6e29e3105
rory-landau-gravwell <rory.landau@gravwell.io>
2026-02-18 12:39:25 -0800

    introduce users list

---
+++ gwcli/tree/users/user.go
@@ -0,0 +1,29 @@
+package users
+
+import (
+	"github.com/gravwell/gravwell/v4/client/types"
+	"github.com/gravwell/gravwell/v4/gwcli/action"
+	"github.com/gravwell/gravwell/v4/gwcli/connection"
+	"github.com/gravwell/gravwell/v4/gwcli/utilities/scaffold/scaffoldlist"
+	"github.com/gravwell/gravwell/v4/gwcli/utilities/treeutils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+func NewUserNav() *cobra.Command {
+	const (
+		use   string = "users"
+		short string = "manage users"
+		long  string = "View and edit properties and properties of users in the system."
+	)
+
+	return treeutils.GenerateNav(use, short, long, nil, nil,
+		[]action.Pair{list()})
+}
+
+func list() action.Pair {
+	return scaffoldlist.NewListAction("list users", "Retrieves details about every user in the system", types.User{},
+		func(fs *pflag.FlagSet) ([]types.User, error) {
+			return connection.Client.GetAllUsers()
+		}, scaffoldlist.Options{})
+}
