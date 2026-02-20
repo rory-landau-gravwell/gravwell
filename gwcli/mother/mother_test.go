d44ca2e0dc110a91180aa2f4339f47af0b7688db
rory-landau-gravwell <rory.landau@gravwell.io>
2026-02-19 17:43:43 -0800

    add TeaTests to Mother for suggestion completion

--- gwcli/mother/mother_test.go
+++ gwcli/mother/mother_test.go
@@ -1,7 +1,6 @@
 package mother_test
 
 import (
-	"io"
 	"os"
 	"path"
 	"strings"
@@ -20,47 +19,70 @@ import (
 	"github.com/spf13/pflag"
 )
 
+// regenerate these golden files with:
+// go test -test.fullpath=true -timeout 30s -run ^Test_SuggestionCompletion* github.com/gravwell/gravwell/v4/gwcli/mother -update
 func Test_SuggestionCompletion_TeaTest(t *testing.T) {
-	t.Run("completion on empty input completes to help", func(t *testing.T) {
-		// initialize singletons
-		logpath := path.Join(t.TempDir(), "log.txt")
-		t.Log("logging to", logpath)
-		clilog.InitializeFromArgs([]string{"--log=" + logpath, "--loglevel=debug"})
-		t.Cleanup(func() {
-			if t.Failed() {
-				if b, err := os.ReadFile(logpath); err != nil {
-					t.Log(err)
-				} else {
-					t.Log("Log Output:\n", string(b))
-				}
-
+	// initialize singletons
+	logpath := path.Join(t.TempDir(), "log.txt")
+	t.Log("logging to", logpath)
+	clilog.InitializeFromArgs([]string{"--log=" + logpath, "--loglevel=debug"})
+	t.Cleanup(func() {
+		if t.Failed() {
+			if b, err := os.ReadFile(logpath); err != nil {
+				t.Log(err)
+			} else {
+				t.Log("Log Output:\n", string(b))
 			}
-		})
-		stylesheet.Cur = stylesheet.Plain()
 
-		// build up some example commands
-		nav1Action1 := scaffold.NewBasicAction("action1", "action1 short", "action1 long",
-			func(cmd *cobra.Command, fs *pflag.FlagSet) (string, tea.Cmd) { return "", nil }, scaffold.BasicOptions{})
-		nav1 := treeutils.GenerateNav("nav1", "nav1 short", "nav1 long", nil, nil, []action.Pair{nav1Action1})
-		nav2 := treeutils.GenerateNav("nav2", "nav2 short", "nav2 long", nil, nil, nil)
-
-		root := treeutils.GenerateNav("root", "root short", "root long", nil,
-			[]*cobra.Command{nav1, nav2}, nil)
-
-		mthr := mother.New(root, root, nil, nil)
-		tm := teatest.NewTestModel(t, mthr, teatest.WithInitialTermSize(100, 80))
-		testsupport.TTSendSpecial(tm, tea.KeyTab)
-		o := tm.Output()
-		b, err := io.ReadAll(o)
-		if err != nil {
-			t.Fatal(err)
 		}
-		// should contain help exactly twice; once for the prompt, onces for the suggestion bars
-		if count := strings.Count(string(b), "help"); count != 2 {
-			t.Errorf("incorrect \"help\" count: %v", testsupport.ExpectedActual(2, count))
-		}
-		teatest.RequireEqualOutput(t, b)
+	})
+	// enable no color
+	stylesheet.Cur = stylesheet.Plain()
+	stylesheet.NoColor = true
+	// build up some example commands
+	nav1Action1 := scaffold.NewBasicAction("actionone", "action1 short", "action1 long",
+		func(cmd *cobra.Command, fs *pflag.FlagSet) (string, tea.Cmd) { return "", nil }, scaffold.BasicOptions{})
+	nav1 := treeutils.GenerateNav("topNav1", "nav1 short", "nav1 long", nil, nil, []action.Pair{nav1Action1})
+	nav2 := treeutils.GenerateNav("topNav2", "nav2 short", "nav2 long", nil, nil, nil)
+	action1 := scaffold.NewBasicAction("topAct", "action1 short", "action1 long",
+		func(cmd *cobra.Command, fs *pflag.FlagSet) (string, tea.Cmd) { return "", nil }, scaffold.BasicOptions{})
+	root := treeutils.GenerateNav("root", "root short", "root long", nil,
+		[]*cobra.Command{nav1, nav2}, []action.Pair{action1})
+
+	mthr := mother.New(root, root, nil, nil)
+	tm := teatest.NewTestModel(t, mthr, teatest.WithInitialTermSize(100, 80))
+	t.Cleanup(func() {
+		testsupport.TTSendSpecial(tm, tea.KeyCtrlC)
 	})
 
-	// TODO
+	t.Run("completion on empty input completes to help", func(t *testing.T) {
+		testsupport.TTSendSpecial(tm, tea.KeyTab)
+
+		out := testsupport.TTMatchGolden(t, tm, false, 0)
+		// should contain help exactly twice; once for the prompt, once for the suggestion bars
+		if count := strings.Count(string(out), "help"); count != 2 {
+			t.Errorf("incorrect \"help\" count: %v", testsupport.ExpectedActual(2, count))
+		}
+	})
+	t.Run("clear prompt on ctrl+u", func(t *testing.T) {
+		testsupport.TTSendSpecial(tm, tea.KeyCtrlU)
+		testsupport.TTMatchGolden(t, tm, false, 0)
+	})
+
+	// The following test does not work properly.
+	// TeaTest is inconsistent with whether or not it sends any characters.
+	// Sometimes "top" and tab are sent and everything works fine.
+	// Sometimes only "top" is sent and the tab completion does not occur.
+	// Sometimes no characters are sent.
+	// This inconsistency makes it difficult to generate a golden file and impossible to diff it with any reliability.
+	/*t.Run("navs are prioritized over actions", func(t *testing.T) {
+		// navs should be sorted alphanumerically, but always suggested before actions
+		tm.Type("top	")
+		testsupport.TTSendSpecial(tm, tea.KeyTab)
+		time.Sleep(100 * time.Millisecond)
+		out := testsupport.TTMatchGolden(t, tm, false, 0)
+		if count := strings.Count(string(out), "topNav1"); count != 2 {
+			t.Error("incorrect suggestion count")
+		}
+	})*/
 }
